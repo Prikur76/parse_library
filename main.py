1b747a8b17acb3ca80b2349aa353627987e8aa54
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-02-08 13:26:30 +0300

    Add check for redirect

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import os
 import requests
 
 
-def fetch_text_from_library(id):
+def fetch_text_from_library(book_id):
     """ TODO """
     txt_url = 'https://tululu.org/txt.php'
     payload = {
-        'id': id
+        'id': book_id
     }
-    response = requests.get(url=txt_url, params=payload)  # verify=False
+    response = requests.get(url=txt_url, params=payload)
     response.raise_for_status()
-    return response.content
+    return response
+
+
+def check_for_redirect(response):
+    """TODO"""
+    if response.history:
+        raise requests.exceptions.HTTPError(response.history[0])
+    return response
 
 
 def download_book(filename, book_text):
@@ -20,16 +27,18 @@ def download_book(filename, book_text):
 
 
 def main():
-
-
     books_path = './books/'
     if not os.path.exists(books_path):
         os.makedirs(books_path)
-
-    for txt_id in range(1, 11):
-        book_text = fetch_text_from_library(txt_id)
-        filename = os.path.join(books_path, f"id{txt_id}.txt")
-        download_book(filename, book_text)
+    for book_id in range(1, 11):
+        try:
+            filename = os.path.join(books_path, f"id{book_id}.txt")
+            response = fetch_text_from_library(book_id)
+            if check_for_redirect(response):
+                book_text = response.content
+                download_book(filename, book_text)
+        except requests.exceptions.HTTPError as err:
+            print(f"book_id {book_id}: {err}")
     return
 
 
