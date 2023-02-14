55a2226bb3f8d71682212c05cf9217ee33319c62
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-02-14 13:08:01 +0300

    Download book cover
    + added several functions:
    1. added fetch_text_url
    2. refactor download_content
    3. added fetch_image_url_and_name
    4. added fetch_title_and_author

--- main.py
+++ main.py
@@ -1,70 +1,114 @@
 import os
+from urllib.parse import urljoin
+
 import requests
 from bs4 import BeautifulSoup
-import re
-from pathvalidate import sanitize_filename, validate_filepath
-
-
-def fetch_text_from_library(url, book_id):
-    """ TODO """
-    payload = {
-        'id': book_id
-    }
-    response = requests.get(url=url, params=payload)
-    print(response.status_code)
-    response.raise_for_status()
-    return response
+from pathvalidate import sanitize_filename
 
 
 def check_for_redirect(response):
-    """TODO"""
+    """Возвращает response если ответ <30Х> (переадресация)"""
     if response.history:
         raise requests.exceptions.HTTPError(response.history[0])
     return response
 
 
-def download_book(filename, book_text):
-    """Записываем файл"""
-    with open(filename, 'wb') as file:
-        file.write(book_text)
+def download_content(url, file_name, folder='books/'):
+    """Функция для скачивания файлов в заданную папку"""
+    try:
+        response = requests.get(url=url)
+        response.raise_for_status()
+        if check_for_redirect(response):
+            filepath = os.path.join(folder, file_name)
+            with open(filepath, 'wb') as file:
+                file.write(content)
+    except requests.exceptions.HTTPError as err:
+        print('Error: ', err)
 
-def download_text(url, book_id, folder='books/'):
-    """Функция для скачивания текстовых файлов."""
-    title_url = 'b'.join([url.split('txt.php')[0], str(book_id)])
-    print(title_url)
-    response = requests.get(url=title_url)
+
+def fetch_text_url(response):
+    """Возвращает словарь тэгов 'a' из ответа сайта"""
     soup = BeautifulSoup(response.text, 'lxml')
-    title_tag = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content').find('h1').text
-    title = title_tag.split(' :: ')[0].strip()
-    filename = sanitize_filename(f'{title}.txt')
-    filepath = os.path.join(folder, filename)
-    return filepath
+    a_tags = []
+    if soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content'):
+        a_tags = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content') \
+            .find('table', class_='d_book').find_all('a')
+    for tag in a_tags:
+        if tag.text == 'скачать txt' and soup.find('table', class_='tabs').find('div', class_='bookimage'):
+            return urljoin(response.url, tag.get('href'))
+
+
+def fetch_image_url_and_name(response):
+    """Возвращает ссылку для скачивания изображения и имя файла"""
+    soup = BeautifulSoup(response.text, 'lxml')
+    a_tags = []
+    if soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content'):
+        a_tags = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content') \
+            .find('table', class_='d_book').find_all('a')
+    for tag in a_tags:
+        if tag.text == 'скачать txt' and soup.find('table', class_='tabs').find('div', class_='bookimage'):
+            image = soup.find('table', class_='tabs').find('div', class_='bookimage').find('img')
+            image_url = urljoin(response.url, image.get('src'))
+            image_name = image.get('src').split('/')[-1]
+            return image_url, image_name
+
+
+def fetch_title_and_author(response):
+    """Возвращает название книги и автора"""
+    soup = BeautifulSoup(response.text, 'lxml')
+    a_tags = []
+    if soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content'):
+        a_tags = soup.find('table', class_='tabs').find('td', class_='ow_px_td').find('div', id='content') \
+            .find('table', class_='d_book').find_all('a')
+    for tag in a_tags:
+        if tag.text == 'скачать txt' and soup.find('table', class_='tabs').find('div', class_='bookimage'):
+            title_tag = soup.find('table', class_='tabs').find('td', class_='ow_px_td')\
+                .find('div', id='content').find('h1').text
+            title = title_tag.split(' :: ')[0].strip()
+            author = title_tag.split(' :: ')[1].strip()
+            return title, author
+
+
+def parse_books_pages(base_url, start_id=1, end_id=2):
+    """Возвращает информацию о книгах в словаре"""
+    books_info = []
+    for book_id in range(start_id, end_id):
+        book_url = urljoin(base_url, f'b{book_id}')
+        response = requests.get(url=book_url)
+        text_url = fetch_text_url(response)
+        if text_url:
+            image_url = fetch_image_url_and_name(response)[0]
+            image_name = fetch_image_url_and_name(response)[1]
+            title = fetch_title_and_author(response)[0]
+            author = fetch_title_and_author(response)[1]
+            book_name = sanitize_filename(f'{title}.txt')
+            books_info.append(
+                {
+                    'book_id': book_id, 'text_url': text_url, 'image_url': image_url, 'image_name': image_name,
+                    'title': title, 'author': author, 'book_name': book_name
+                }
+            )
+    return books_info
 
 
 def main():
-    url = 'https://tululu.org/txt.php'
     books_path = './books/'
     if not os.path.exists(books_path):
         os.makedirs(books_path)
-    for book_id in range(1, 11):
-        try:
-            response = fetch_text_from_library(url, book_id)
-            if check_for_redirect(response):
-                book_text = response.content
-                filename = download_text(url, book_id)
-                download_book(filename, book_text)
-        except requests.exceptions.HTTPError as err:
-            print(f"book_id {book_id}: {err}")
-    return
+
+    image_path = './images/'
+    if not os.path.exists(image_path):
+        os.makedirs(image_path)
+
+    base_url = 'https://tululu.org'
+    start_id = 1
+    end_id = 31
+    books = parse_books_pages(base_url, start_id=start_id, end_id=end_id)
+    for number, book in enumerate(books, start=1):
+        print(f"Книга № {number}.\nНазвание: {book['title']}.\nАвтор: {book['author']}.\n")
+        download_text(book['text_url'], book['book_name'], folder='books/')
+        download_text(book['image_url'], book['image_name'], folder='images/')
 
 
 if __name__ == '__main__':
     main()
-
-    # url = 'https://tululu.org/txt.php'
-    # book_id = '1'
-    # url_1 = 'b'.join([url.split('txt.php')[0], book_id])
-    # print(url_1)
-
-    # filename = download_text(url, book_id)
-    # print(filename)
