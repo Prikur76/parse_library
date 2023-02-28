0e59a9b3cfe650dc7acd29f5484730edfb077d1f
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-02-28 18:02:45 +0300

    Updated parse_tululu_category.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,29 +2,24 @@ import logging
 import os
 import re
 import time
+import json
 
 import requests
 from bs4 import BeautifulSoup
 
-import tools as t
+import tools
 
 logger = logging.getLogger(__name__)
 
 
-def fetch_pages_count(soup):
-    """Возвращает количество страниц"""
-    pages_count = int(soup.find_all('a', class_='npage')[-1].text)
-    return pages_count
-
-
-def fetch_books_ids(soup):
-    """Возвращает список номеров книг"""
+def get_books_ids_from_pages(base_url, start_page=1, end_page=2):
+    """Возвращает список ID книг на выбранных страницах"""
     books_ids = []
-    a_tags = soup.find('div', id='content')\
-        .find_all('a', title=re.compile('скачать'))
-    for tag in a_tags:
-        book_id = re.findall(r'(\d+)', tag.get('href'))[0]
-        books_ids.append(book_id)
+    for page in range(start_page, end_page):
+        sci_fi_addition = f'/l55/{page}/'
+        response = tools.get_response(base_url, sci_fi_addition)
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_ids.extend(tools.fetch_books_ids(soup))
     return books_ids
 
 
@@ -38,37 +33,68 @@ def main():
     image_path = './images/'
     os.makedirs(books_path, exist_ok=True)
     os.makedirs(image_path, exist_ok=True)
-
     base_url = 'https://tululu.org/'
-    pages_count = 11
-    books = []
-    for page in range(1, pages_count, 1):
-        sci_fi_addition = f'/l55/{page}/'
-        response = t.get_response(base_url, sci_fi_addition)
-        soup = BeautifulSoup(response.text, 'lxml')
-        # pages_count = fetch_pages_count(soup)
-        books_ids = fetch_books_ids(soup)
 
-        for book_id in books_ids:
-            try:
-                book_addition = f'/b{book_id}/'
-                response = t.get_response(base_url, book_addition)
-                book = t.parse_book_page(response, book_id)
-                books.append(book)
-                t.download_content(book['text_url'], book['book_name'],
+    # parser = argparse.ArgumentParser(
+    #     description='Скачивание книг из библиотеки  tululu.org'
+    # )
+    # parser.add_argument('--start_page', type=int, default=1,
+    #                     help='Ввести номер первой страницы')
+    # parser.add_argument('--end_page', type=int, default=None,
+    #                     help='Ввести номер последней страницы')
+    # args = parser.parse_args()
+    # start_id = args.start_page
+    # end_id = args.end_page
+
+    try:
+        base_url = 'https://tululu.org/'
+        all_books_ids = get_books_ids_from_pages(base_url, start_page=1, end_page=4)
+        # sci_fi_addition = f'/l55/1/'
+        # response = tools.get_response(base_url, sci_fi_addition)
+        # soup = BeautifulSoup(response.text, 'lxml')
+        # pages_count = 4                  # tools.fetch_pages_count(soup)
+        # books_ids = tools.fetch_books_ids(soup)
+        # for page in range(2, pages_count+1, 1):
+        #     sci_fi_addition = f'/l55/{page}/'
+        #     response = tools.get_response(base_url, sci_fi_addition)
+        #     soup = BeautifulSoup(response.text, 'lxml')
+        #     books_ids.extend(tools.fetch_books_ids(soup))
+
+    except requests.exceptions.ConnectionError as connection_err:
+        logger.error(f"Lost HTTP connection: {connection_err}")
+        time.sleep(10)
+
+    books = []
+    for book_id in all_books_ids:
+        try:
+            book_addition = f'/b{book_id}/'
+            response = tools.get_response(base_url, book_addition)
+            book = tools.parse_book_page(response, book_id)
+            books.append(book)
+            tools.download_content(book['text_url'], book['book_name'],
                                    folder='books/')
-                t.download_content(book['image_url'], book['image_name'],
+            tools.download_content(book['image_url'], book['image_name'],
                                    folder='images/')
-            except AttributeError as attr_err:
-                logger.error(f"BOOK ID: {book_id} -> AttributeError: {attr_err}")
-            except requests.exceptions.HTTPError as http_err:
-                logger.error(f"BOOK ID: {book_id} -> HTTPError: {http_err}")
-            except requests.exceptions.ConnectionError as connection_err:
-                logger.error(f"Lost HTTP connection: {connection_err}")
-                time.sleep(10)
-    t.publish_books_to_console(books)
+        except AttributeError as attr_err:
+            logger.error(f"BOOK ID: {book_id} -> AttributeError: {attr_err}")
+        except requests.exceptions.HTTPError as http_err:
+            logger.error(f"BOOK ID: {book_id} -> HTTPError: {http_err}")
+        except requests.exceptions.ConnectionError as connection_err:
+            logger.error(f"Lost HTTP connection: {connection_err}")
+            time.sleep(10)
+    # tools.publish_books_to_console(books)
+    tools.download_books_to_file(books)
+
     return
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    print(type('books.json'))
+
+    # with open("books.json", "r") as my_file:
+    #     books_json = my_file.read()
+    #     print(type(books_json))
+    #
+    # read_books = json.loads(books_json)
+    # print(read_books)
