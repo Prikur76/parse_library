7139732ed4f7c1efb338111663aeae888e577404
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-03 16:54:48 +0300

    Updated parse_tululu_category.py.py

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,23 +1,14 @@
+import argparse
 import logging
 import os
 import time
 from urllib.parse import urljoin
 
 import requests
-from bs4 import BeautifulSoup
 
 import tools
 
 logger = logging.getLogger(__name__)
-
-
-def collects_books_ids(page_url):
-    """Возвращает список ID книг на указанной странице"""
-    response = tools.get_response(page_url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, 'lxml')
-    return tools.fetch_books_ids_from_page(soup)
-
 
 
 def main():
@@ -25,59 +16,82 @@ def main():
         format="[%(levelname)s] - %(asctime)s - %(name)s - %(message)s",
         level=logging.INFO
     )
-
-    books_path = './books/'
-    image_path = './images/'
-    os.makedirs(books_path, exist_ok=True)
-    os.makedirs(image_path, exist_ok=True)
-    base_url = 'https://tululu.org/'
+    dir_path = os.path.dirname(os.path.realpath(__file__))
 
     parser = argparse.ArgumentParser(
-        description='Скачивание книг из библиотеки  tululu.org'
+        description="""\
+        Скачивание книг из раздела научной фантастики библиотеки tululu.org
+        """
     )
     parser.add_argument('--start_page', type=int, default=1,
                         help='Ввести номер первой страницы')
-    parser.add_argument('--end_page', type=int, default=None,
+    parser.add_argument('--end_page', type=int, default=1,
                         help='Ввести номер последней страницы')
+    parser.add_argument('--dest_folder', default=dir_path,
+                        help='Указать путь к каталогу с результатами парсинга')
+    parser.add_argument('--json_path', default=dir_path,
+                        help='Указать свой путь к *.json файлу с результатами')
+    parser.add_argument('--skip_imgs', action='store_true',
+                        help='Hе скачивать картинки')
+    parser.add_argument('--skip_txt', action='store_true',
+                        help='Не скачивать книги')
+
     args = parser.parse_args()
     start = args.start_page
-    end = args.end_page + 1
+    end = args.end_page
+    if start >= end:
+        end = tools.get_last_page_number() + 1
+    skip_txt = args.skip_txt
+    skip_imgs = args.skip_imgs
+    destination = args.dest_folder
+    books_file_path = args.json_path
+
+    books_path = os.path.join(destination, 'books\\')
+    images_path = os.path.join(destination, 'images\\')
+
+    os.makedirs(books_path, exist_ok=True)
+    os.makedirs(images_path, exist_ok=True)
 
     base_url = 'https://tululu.org/'
 
-    books_ids = []
-    for page in range(start, end):
+    books_urls = []
+    for page in range(start, end, 1):
         try:
+
             page_url = urljoin(base_url, f'/l55/{page}/')
-            books_ids.append(collects_books_ids(page_url))
+            page_books_urls = tools.fetch_books_urls_from_page(page_url)
+            books_urls.extend(page_books_urls)
+
         except requests.exceptions.HTTPError as http_err:
             logger.error(f"Request failed with HTTPError: {http_err}")
+
         except requests.exceptions.ConnectionError as connection_err:
             logger.error(f"Lost HTTP connection: {connection_err}")
             time.sleep(10)
 
     books = []
-    for book_id in books_ids:
+    for book_url in books_urls:
         try:
-            book_url = urljoin(base_url, f'/b{book_id}/')
             response = tools.get_response(book_url)
-            book = tools.parse_book_page(response, book_id)
+            book = tools.parse_book_page(response)
             books.append(book)
-            tools.download_content(book['text_url'], book['book_name'],
-                                   folder='books/')
-            tools.download_content(book['image_url'], book['image_name'],
-                                   folder='images/')
-        except AttributeError as attr_err:
-            logger.error(f"BOOK ID: {book_id} -> AttributeError: {attr_err}")
+            if not skip_txt:
+                tools.download_content(book['text_url'], book['book_name'],
+                                       folder=books_path)
+            if not skip_imgs:
+                tools.download_content(book['image_url'], book['image_name'],
+                                       folder=images_path)
+            print(book_url)
+        except TypeError as type_err:
+            logger.error(f"BOOK: {book_url} -> TypeError: {type_err}")
         except requests.exceptions.HTTPError as http_err:
-            logger.error(f"BOOK ID: {book_id} -> HTTPError: {http_err}")
+            logger.error(f"BOOK: {book_url} -> HTTPError: {http_err}")
         except requests.exceptions.ConnectionError as connection_err:
             logger.error(f"Lost HTTP connection: {connection_err}")
             time.sleep(10)
-
+    #
     tools.publish_books_to_console(books)
-    tools.download_books_to_file(books)
-
+    tools.download_books_to_file(books, books_file_path)
     return
 
 
