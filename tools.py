3d1bb93ca47836dd5720e269b941bf0a8cc2a691
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-06 12:25:53 +0300

    Fixed errors in tools

--- tools.py
+++ tools.py
@@ -26,46 +26,49 @@ def fetch_text_url(url, soup):
     или возбуждает исключение TypeError
     """
     tag_href = soup.select_one('table.d_book a[href*="/txt.php?id="]')
-    if tag_href is None:
-        raise exceptions.MyCustomError('No txt tag')
-    return urljoin(url, tag_href.get('href'))
+    if tag_href:
+        return urljoin(url, tag_href.get('href'))
+    raise exceptions.NoTagError('No txt tag')
+
 
 
 def fetch_image_url_and_name(url, soup):
     """Возвращает ссылку для скачивания изображения и имя файла"""
     image = soup.select_one('div.bookimage img')
-    if image is None:
-        raise exceptions.MyCustomError('No image tag')
-    image_url = urljoin(url, image.get('src'))
-    image_name = image_url.split('/')[-1]
-    return image_url, image_name
-
+    if image:
+        image_url = urljoin(url, image.get('src'))
+        image_name = image_url.split('/')[-1]
+        return image_url, image_name
+    raise exceptions.NoTagError('No image tag')
 
 def fetch_title_and_author(soup):
     """Возвращает название книги и автора"""
     title_tag = soup.select_one('h1').text
-    if title_tag is None:
-        raise exceptions.MyCustomError('No title text')
-    title, author = title_tag.split(' :: ')
-    return title.strip(), author.strip()
+    if title_tag:
+        title, author = title_tag.split(' :: ')
+        return title.strip(), author.strip()
+    raise exceptions.NoTagError('No title text')
+
 
 
 def fetch_genres(soup):
     """Возвращает жанр книги или пустой список"""
     genres_tags = soup.select('span.d_book a')
-    if genres_tags is None:
-        raise exceptions.MyCustomError('No genres tag')
-    genres = [genre.text for genre in genres_tags]
-    return genres
+    if genres_tags:
+        genres = [genre.text for genre in genres_tags]
+        return genres
+    raise exceptions.NoTagError('No genres tag')
+
 
 
 def fetch_comments(soup):
     """Возвращает список комментариев или пустой список"""
     comments_tags = soup.select('div.texts span')
-    if comments_tags is None:
-        raise exceptions.MyCustomError('No comments tag')
-    comments = [comment.text for comment in comments_tags]
-    return comments
+    if comments_tags:
+        comments = [comment.text for comment in comments_tags]
+        return comments
+    raise exceptions.NoTagError('No comments tag')
+
 
 
 def parse_book_page(response):
@@ -148,9 +151,10 @@ def get_last_page_number():
     response = get_response(sci_fi_url)
     soup = BeautifulSoup(response.text, 'lxml')
     last_page_number = int(soup.select('a.npage')[-1].text)
-    if last_page_number is None:
-        raise exceptions.MyCustomError("No 'last page' tag")
-    return last_page_number
+    if last_page_number:
+        return last_page_number
+    raise exceptions.NoTagError("No 'last page' tag")
+
 
 
 def fetch_books_urls_from_page(page_url):
@@ -158,7 +162,8 @@ def fetch_books_urls_from_page(page_url):
     response = get_response(page_url)
     soup = BeautifulSoup(response.text, 'lxml')
     a_tags = soup.select('div.bookimage > a')
-    if a_tags is None:
-        raise exceptions.MyCustomError('No book url tag')
-    page_books_urls = [urljoin(page_url, tag['href']) for tag in a_tags]
-    return page_books_urls
+    if a_tags:
+        page_books_urls = [urljoin(page_url, tag['href']) for tag in a_tags]
+        return page_books_urls
+    raise exceptions.NoTagError('No book url tag')
+
