2e917c17bd5a6731ab3ccfd9e638f70293337d4e
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-06 18:10:08 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,24 +1,33 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
+def on_reload():
+    """ """
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+    with open("books.json", encoding='utf-8', errors='ignore') as file:
+        books = json.load(file, strict=False)
 
-with open("books.json", encoding='utf-8', errors='ignore') as json_data:
-    books = json.load(json_data, strict=False)
+    template = env.get_template('template.html')
+    rendered_page = template.render(books=books)
+    with open('index.html', 'w', encoding="utf8") as index_html:
+        index_html.write(rendered_page)
 
 
-template = env.get_template('template.html')
-rendered_page = template.render(books=books)
+def rebuild():
+    on_reload()
+    print("Site rebuilt")
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+rebuild()
+server = Server()
+server.watch('template.html', rebuild)
+server.serve(root='.')
 
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+# server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+# server.serve_forever()
