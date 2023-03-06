3ffd7efa9df6aac2e3432f41259c74b1a551b7b5
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-06 15:42:00 +0300

    Init render_website.py

---
+++ render_website.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+
+load_dotenv()
+
+env = Environment(loader=FileSystemLoader('.'),
+                  autoescape=select_autoescape(['html', 'xml']))
+
+template = env.get_template('template.html')
+
+rendered_page = template.render(foundation_text=foundation_text, wines=wines)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
+
+
+server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server.serve_forever()
