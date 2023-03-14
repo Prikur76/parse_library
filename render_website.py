4a78ec715ce40ba0582c8a67a62cc96c1a38b4f5
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2023-03-14 12:33:18 +0300

    Fixed render_website.py

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 
@@ -6,23 +7,27 @@ from livereload import Server
 from more_itertools import chunked
 
 
-def on_reload():
-    """Возвращает папку pages с файлами html"""
+def on_reload(books_source='books.json'):
+    """Возвращает папку html_pages с файлами html"""
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    with open("books.json", encoding='utf-8', errors='ignore') as file:
-        deserialized_file = json.load(file, strict=False)
-        pages = list(chunked(deserialized_file, 20))
-        pages_count = len(pages)
+    with open(books_source, encoding='utf-8', errors='ignore') as file:
+        books_descriptions = json.load(file, strict=False)
+
+    books_count_on_page = 20
+    html_pages = list(chunked(books_descriptions,
+                              books_count_on_page))
+
+    pages_count = len(html_pages)
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    pages_path = os.path.join(dir_path, 'pages')
+    pages_path = os.path.join(dir_path, 'html_pages')
     os.makedirs(pages_path, exist_ok=True)
 
-    template = env.get_template('template.html')
-    for page_number, page in enumerate(pages, start=1):
+    template = env.get_template('static/template.html')
+    for page_number, page in enumerate(html_pages, start=1):
         index_html_path = f'{pages_path}/index{page_number}.html'
         books_pages = [
             {
@@ -37,12 +42,23 @@ def on_reload():
             index_html.write(rendered_page)
 
 
-def rebuild():
-    on_reload()
-    print("Site rebuilt")
+def main():
+    parser = argparse.ArgumentParser(
+        description="""\
+            Запуск отладочного веб-сервера библиотеки
+            """
+    )
+    parser.add_argument('-bs', '--books_source', default='books.json',
+                        help='Ввести путь к файлу с описанием книг')
+    args = parser.parse_args()
+    books_source = args.books_source
+
+    on_reload(books_source)
+
+    server = Server()
+    server.watch('../static/template.html', on_reload)
+    server.serve(root='.', default_filename='index.html')
 
 
-rebuild()
-server = Server()
-server.watch('template.html', rebuild)
-server.serve(root='.', default_filename='./pages/index1.html')
+if __name__ == '__main__':
+    main()
